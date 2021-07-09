7fc1d8ce55ae738e974e87942c5de91ef8ca7805
Zach Lynn <zachlynn@Zachs-iMac.local>
2021-07-09 16:34:18 -0400

    JS

---
+++ script.js
@@ -0,0 +1,6 @@
+const resourcePage = document.querySelector('.resourcePage');
+
+
+resourcePage.addEventListener('click', function() {
+   alert("Greetings! If you have a great music resource to share, send it to sonetto47104@gmail.com  -Thanks!")
+})
