7ec22eef63d977e6c900fddc28f1cd9d2076079c
Zach Lynn <zachlynn@Zachs-iMac.local>
2021-07-13 22:00:17 -0400

    formatAndJs

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const resourcePage = document.querySelector('.resourcePage');
-
+const classicalInfo = document.querySelector('.classical')
 
 resourcePage.addEventListener('click', function() {
    alert("Greetings! If you have a great music resource to share, send it to sonetto47104@gmail.com  -Thanks!")
+});
+
+classicalInfo.addEventListener('click', function() {
+   alert("The term 'classical music' is often used in refernce to many different periods of wetern music, including the 'Classical period' among others.")
 })
